238e4985eb26c49c41813595dfbea3b4f9836685
NklsGremory <67396914+NklsGremory@users.noreply.github.com>
2025-03-24 21:31:22 -0300

    add an icon

--- test.py
+++ test.py
@@ -9,20 +9,31 @@ def main (page: ft.Page):
     #funções
 
     def gerarSenha(e):
-        print("Função funcionando!", campoEntrada.value)
+        print("Função funcionando!", qntdElements.value)
+
+        page.update()
+
+    def atualizarPag(e):
 
         page.update()
 
 
     # criação dos elementos
-    titulo = ft.Text("Password Generator", size=20, color="black")
-    campoEntrada = ft.TextField("Digite o tamanho da senha")
-    botao = ft.Button("Gerar Senha", on_click=gerarSenha)
+    titulo = ft.Text("Password Generator", size=35, color="white")
+
+    textPage = ft.Text("Informe os parâmetrs para gerar a senha", size=18, color="white")
+    qntdElements = ft.TextField(label="Digite o tamaho da senha")
+    cSymbols = ft.Checkbox(label="Deve conter símbolos")
+    cNumbers = ft.Checkbox(label="Deve conter números")
+    cAllLetters = ft.Checkbox(label="Deve conter todas as letras, maiúsculas e minúsculas")
+    btnSend = ft.Button("Gerar Senha", on_click=gerarSenha)
+
     senha = ft.Text("")
+    btnCopy = ft.Button(icon= ft.icons.COPY_ROUNDED)
 
 
     #adiciona os elementos na página
-    page.add(titulo, campoEntrada, botao, senha)
+    page.add(titulo,textPage, qntdElements, cSymbols, cNumbers, cAllLetters, botao, senha)
 
 
 
