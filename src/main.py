69dc3754743de9f95b32f3c87ba23788c7b77764
NklsGremory <67396914+NklsGremory@users.noreply.github.com>
2025-03-27 16:34:44 -0300

    melhoria layaout / default bug solved

--- src/main.py
+++ src/main.py
@@ -22,10 +22,11 @@ def main (page: ft.Page):
         letrasMi = "abcdefghijklmnopqrstuvwxyz"
         numeros = "0123456789"
         simbolos = "!@#$%&*()_+"
-        param = numeros # default case
+        param = ""
         senha = ""
 
-        param = numeros if(cNumbers.value) else ""
+        if(cNumbers.value == False and cSymbols.value == False and cLettersMa.value == False and cLettersMi.value == False):
+            param = numeros
         param += simbolos if(cSymbols.value) else ""
         param += letrasMa if(cLettersMa.value) else ""
         param += letrasMi if(cLettersMi.value) else ""
@@ -40,17 +41,16 @@ def main (page: ft.Page):
         page.set_clipboard(senha.value)
         print("Senha copiada!") # test no terminal
 
-        page.open(ft.SnackBar(ft.Text("Senha copiada com sucesso!"), duration=2500))
+        page.open(ft.SnackBar(ft.Text("Senha copiada com sucesso para a área de transferência!", size= 17, color=colors["green"], style= ft.TextStyle(weight=ft.FontWeight.BOLD), text_align= ft.TextAlign.CENTER), duration=2000, bgcolor=colors["darkBlue"]))
 
         page.update()
 
     def updateProgram(e): 
         if(qntdElements.value == ""):
-            page.open(ft.SnackBar(ft.Text("Por favor, informe o tamanho da senha!"), duration=2500))
+            page.open(ft.SnackBar(ft.Text("Por favor, informe o tamanho da senha!", size= 18, color=colors["green"], style= ft.TextStyle(weight=ft.FontWeight.BOLD), text_align= ft.TextAlign.CENTER), duration=2500, bgcolor=colors["darkBlue"]))
             qntdElements.focus()
         else:
             senha.value = criadorSenhas(int(qntdElements.value))
-
             page.remove(line2Base)
             page.add(line2Updated)
             textPage.value = "Senha Gerada:"
@@ -66,7 +66,7 @@ def main (page: ft.Page):
         
     # criação dos elementos
     titulo = ft.Text("Password Generator", size=35, color="white")
-    btnBack = ft.IconButton(icon=ft.Icons.ARROW_BACK, on_click=voltarInicio)
+    btnBack = ft.IconButton(icon=ft.Icons.ARROW_BACK, on_click=voltarInicio, icon_color=colors["blue"], icon_size=30)
     logo = ft.Image("icon.png", width=60, height=60, fit= ft.ImageFit.CONTAIN)
     divider = ft.Divider(color= colors["blue"], thickness= 3) #Terminar o divider
 
@@ -78,8 +78,8 @@ def main (page: ft.Page):
     cLettersMi = ft.Checkbox(label="Deve conter letras minúsculas", active_color=colors["green"])
     btnSend = ft.ElevatedButton("Gerar Senha", on_click=updateProgram, style = ft.ButtonStyle(text_style= ft.TextStyle(size= 18)), width= 200, height= 50, color=colors["blue"])
 
-    senha = ft.Text("", size=18, color="white")
-    btnCopy = ft.IconButton(icon=ft.Icons.COPY_ALL_ROUNDED, on_click=copiarSenha) # REVER
+    senha = ft.Text("", size=18, color="white", width= 280, text_align= ft.TextAlign.CENTER)
+    btnCopy = ft.IconButton(icon=ft.Icons.COPY_ALL_ROUNDED, on_click=copiarSenha, icon_color=colors["green"]) # REVER
 
     #variáveis de layout
     lineInicial = ft.Row(
@@ -98,7 +98,7 @@ def main (page: ft.Page):
     )
 
     line2Updated = ft.Column(
-        [ ft.Row([senha, btnCopy], vertical_alignment= ft.CrossAxisAlignment.CENTER), btnBack],
+        [senha, ft.Row([btnBack, btnCopy], vertical_alignment= ft.CrossAxisAlignment.CENTER)],
         horizontal_alignment= ft.CrossAxisAlignment.CENTER
     )
 
@@ -107,7 +107,6 @@ def main (page: ft.Page):
 
 ft.app(main, assets_dir="assets")
 
-#REVER O CASO DEFAULT  --> ALTA PRIORIDADE
 #Organizar o layout
     #ajustar as "paginas"
 #definir cores e fontes
